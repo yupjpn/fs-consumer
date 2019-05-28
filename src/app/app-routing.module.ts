1bbcac6c7f6c62df832aaada34f6226cb5edd254
yupjpn <yuki.peters@duke.edu>
2019-05-28 17:18:22 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule'},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule'}
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
