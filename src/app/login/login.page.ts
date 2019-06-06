de8ffaf90425ec214e816e710afb67afa498546a
yupjpn <yuki.peters@duke.edu>
2019-06-07 00:39:07 +0100

    LOGIN WORKS

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -16,16 +16,32 @@ export class LoginPage implements OnInit {
     password: ""
   }
 
-  constructor(private navCtrl: NavController, private httpClient: HttpClient) { }
+  constructor(private navCtrl: NavController, private httpClient: HttpClient,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+    message: 'Error',
+    subHeader: 'Please try again later.',
+    buttons: ['Dismiss']
+   });
+   await alert.present(); 
+}
+  
   login() {
 
     this.httpClient.post("http://localhost:3000/api/users/authentication", this.user).subscribe(
       (response) => {
         console.log(response);
+        if (response) {
+          this.navToTabs();
+        }
+        else {
+          this.presentAlert();
+        }
       }
     );
 
